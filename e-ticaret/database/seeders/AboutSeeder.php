a7fe623ee847d4523cd7cd1108a0a18160974ac7
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-03-12 12:23:27 +0300

    veritabanına about modeli eklendi ve about sayfası burdan gelen veriler ile dolduruldu.

---
+++ e-ticaret/database/seeders/AboutSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\About;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AboutSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        About::create([
+            'name' => 'Toprak Shop E-Ticaret',
+            'content' => 'Hakkımızda Yazısı Burada',
+            'text_1_icon' => 'icon-truck',
+            'text_1' => 'Ücretsiz Kargo',
+            'text_1_content' => 'Ürünleriniz ücretsiz ve güvenli bir şekilde evinize teslim edilir.',
+            'text_2_icon' => 'icon-refresh2',
+            'text_2' => 'Geri İade',
+            'text_2_content' => '30 gün içerisinde ürünlerinizi iade edebilirsiniz.',
+            'text_3_icon' => 'icon-help',
+            'text_3' => 'Müşteri Destek',
+            'text_3_content' => '/24 bize ulaşabilirsiniz.',
+        ]);
+    }
+}
--- e-ticaret/resources/views/frontend/pages/about.blade.php
+++ e-ticaret/resources/views/frontend/pages/about.blade.php
@@ -14,7 +14,7 @@
             <div class="col-md-6">
                 <div class="block-16">
                     <figure>
-                        <img src="images/blog_1.jpg" alt="Image placeholder" class="img-fluid rounded">
+                        <img src="{{$about->image ?? 'images/blog_1.jpg'}}" alt="Image placeholder" class="img-fluid rounded">
                         <a href="https://vimeo.com/channels/staffpicks/93951774" class="play-button popup-vimeo"><span class="ion-md-play"></span></a>
 
                     </figure>
@@ -25,84 +25,15 @@
 
 
                 <div class="site-section-heading pt-3 mb-4">
-                    <h2 class="text-black">How We Started</h2>
+                    <h2 class="text-black">{{$about->name}}</h2>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius repellat, dicta at laboriosam, nemo exercitationem itaque eveniet architecto cumque, deleniti commodi molestias repellendus quos sequi hic fugiat asperiores illum. Atque, in, fuga excepturi corrupti error corporis aliquam unde nostrum quas.</p>
-                <p>Accusantium dolor ratione maiores est deleniti nihil? Dignissimos est, sunt nulla illum autem in, quibusdam cumque recusandae, laudantium minima repellendus.</p>
+                <p>{!! $about->content !!}</p>
 
             </div>
         </div>
     </div>
 </div>
 
-<div class="site-section border-bottom" data-aos="fade">
-    <div class="container">
-        <div class="row justify-content-center mb-5">
-            <div class="col-md-7 site-section-heading text-center pt-4">
-                <h2>The Team</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-6 col-lg-3">
-
-                <div class="block-38 text-center">
-                    <div class="block-38-img">
-                        <div class="block-38-header">
-                            <img src="images/person_1.jpg" alt="Image placeholder" class="mb-4">
-                            <h3 class="block-38-heading h4">Elizabeth Graham</h3>
-                            <p class="block-38-subheading">CEO/Co-Founder</p>
-                        </div>
-                        <div class="block-38-body">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio recusandae doloribus ut fugit officia voluptate soluta. </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3">
-                <div class="block-38 text-center">
-                    <div class="block-38-img">
-                        <div class="block-38-header">
-                            <img src="images/person_2.jpg" alt="Image placeholder" class="mb-4">
-                            <h3 class="block-38-heading h4">Jennifer Greive</h3>
-                            <p class="block-38-subheading">Co-Founder</p>
-                        </div>
-                        <div class="block-38-body">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio recusandae doloribus ut fugit officia voluptate soluta. </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3">
-                <div class="block-38 text-center">
-                    <div class="block-38-img">
-                        <div class="block-38-header">
-                            <img src="images/person_3.jpg" alt="Image placeholder" class="mb-4">
-                            <h3 class="block-38-heading h4">Patrick Marx</h3>
-                            <p class="block-38-subheading">Marketing</p>
-                        </div>
-                        <div class="block-38-body">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio recusandae doloribus ut fugit officia voluptate soluta. </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3">
-                <div class="block-38 text-center">
-                    <div class="block-38-img">
-                        <div class="block-38-header">
-                            <img src="images/person_4.jpg" alt="Image placeholder" class="mb-4">
-                            <h3 class="block-38-heading h4">Mike Coolbert</h3>
-                            <p class="block-38-subheading">Sales Manager</p>
-                        </div>
-                        <div class="block-38-body">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aut minima nihil sit distinctio recusandae doloribus ut fugit officia voluptate soluta. </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 
 <div class="site-section site-section-sm site-blocks-1 border-0" data-aos="fade">
@@ -110,29 +41,29 @@
         <div class="row">
             <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="">
                 <div class="icon mr-4 align-self-start">
-                    <span class="icon-truck"></span>
+                    <span class="{{$about->text_1_icon}}"></span>
                 </div>
                 <div class="text">
-                    <h2 class="text-uppercase">Free Shipping</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.</p>
+                    <h2 class="text-uppercase">{{$about->text_1}}</h2>
+                    <p>{!! $about->text_1_content !!}</p>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="100">
                 <div class="icon mr-4 align-self-start">
-                    <span class="icon-refresh2"></span>
+                    <span class="{{$about->text_2_icon}}"></span>
                 </div>
                 <div class="text">
-                    <h2 class="text-uppercase">Free Returns</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.</p>
+                    <h2 class="text-uppercase">{{$about->text_2}}</h2>
+                    <p>{!! $about->text_2_content !!}</p>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="200">
                 <div class="icon mr-4 align-self-start">
-                    <span class="icon-help"></span>
+                    <span class="{{$about->text_3_icon}}"></span>
                 </div>
                 <div class="text">
-                    <h2 class="text-uppercase">Customer Support</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.</p>
+                    <h2 class="text-uppercase">{{$about->text_3}}</h2>
+                    <p>{!! $about->text_3_content !!}</p>
                 </div>
             </div>
         </div>
