8d7a7488f1eabeb179ea939fddbe250498e65657
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-03-18 16:35:07 +0300

    düzenleme.

--- e-ticaret/resources/views/frontend/inc/header.blade.php
+++ e-ticaret/resources/views/frontend/inc/header.blade.php
@@ -24,7 +24,7 @@
                             <li>
                                 <a href="{{route('cart')}}" class="site-cart">
                                     <span class="icon icon-shopping_cart"></span>
-                                    <span class="count">{{count(session('cart'))}}</span>
+                                    <span class="count">{{session()->get('cart') ? count(session('cart')) : 0}}</span>
                                 </a>
                             </li>
                             <li class="d-inline-block d-md-none ml-md-0"><a href="#" class="site-menu-toggle js-menu-toggle"><span class="icon-menu"></span></a></li>
